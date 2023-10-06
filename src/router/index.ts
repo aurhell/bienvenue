8fc1df6941744be6ad36ecd8e645ce687deade56
Aurélien Girault <me@agirault.com>
2023-10-06 21:58:45 +0200

    :wrench: Chore: add .nvmrc

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/pages/Home.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
